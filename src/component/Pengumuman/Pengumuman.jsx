67bbe0f3d8d6cca9d5854f0d618a5347e1a4e13d
wawanharianto <wawanharianto696@gmail.com>
2022-12-13 10:30:54 +0800

    repair style

--- src/component/Pengumuman/Pengumuman.jsx
+++ src/component/Pengumuman/Pengumuman.jsx
@@ -5,7 +5,66 @@ function Pengumuman() {
   return (
     <>
       <article className="conPengumuman">
-        <h1>PENGUMUMAN</h1>
+        <div className="headPengumuman">
+          <h1>PENGUMUMAN</h1>
+          <hr />
+        </div>
+        <table className="tPengumuman">
+          <tr>
+            <th>No</th>
+            <th>Organisasi Mahasiswa</th>
+            <th>Nama Kegiatan</th>
+            <th>Keterangan</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+          <tr>
+            <td>6.</td>
+            <td>Badan Eksekutif mahasiswa</td>
+            <td>Sosialisasi Kesehatan pada masyarakat selong lombok barat</td>
+            <td>
+              <span>status</span>
+            </td>
+          </tr>
+        </table>
       </article>
     </>
   );
