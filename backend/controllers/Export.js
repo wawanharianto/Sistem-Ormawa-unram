9472eeb3c3957f44e05fc8be884fa97a0d51d6a2
briandsmth <briandsmith22@gmail.com>
2022-12-16 08:44:39 +0800

    Update export function

--- backend/controllers/Export.js
+++ backend/controllers/Export.js
@@ -14,67 +14,77 @@ export const ExportToExcel = async (req, res) => {
       attributes: ['username', 'email']
     }],
     // where: {
-    //   createAt: ,
+    //   createAt: [req.body.tanggal1 , ],
     // }
   });
 
   if (!proposal) return res.status(404).json({ msg: "Data tidak ditemukan" });
 
-  const workSheetColumnName = [
-    // "Nama Kegiatan",
-    // "Nama Organisasi",
-    // "Jumlah Dana",
-    // "Ketua Panitia",
-    // "Tanggal Pelaksanaan",
-    // "Tempat Pelaksanaan",
-    // "Dana Di setujui"
+  try {
 
-    { v: "Nama Kegiatan", t: "m", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
-    { v: "Nama Organisasi", t: "m", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
-    { v: "Jumlah Dana", t: "m", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
-    { v: "Ketua Panitia", t: "m", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
-    { v: "Tanggal Pelaksanaan", t: "m", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
-  ];
+    const rows = proposal.map(row => ({
+      nama_kegiatan: row.nama_kegiatan,
+      nama_organisasi: row.nama_organisasi,
+      jumlah_dana: row.jumlah_dana,
+      ketua_panitia: row.ketua_panitia,
+      tanggal_pelaksanaan: row.tanggal_pelaksanaan,
+      tempat_pelaksanaan: row.tempat_pelaksanaan,
+    }));
 
-  const workSheetName = 'Data Kegiatan';
-  const filepath = './data.xlsx'
-
-  const exportDataToExcel = (proposal, workSheetColumnName, workSheetName, filepath) => {
-    const data = proposal.map(data => {
-      return [data.nama_kegiatan,
-      data.nama_organisasi,
-      data.jumlah_dana,
-      data.ketua_panitia,
-      data.tanggal_pelaksanaan,
-        // data.tempat_pelaksanaan,
-        // data.dana_disetujui,
-      ]
-    });
-
-    const max_width = data.reduce((w, r) => Math.max(w, r.proposal.nama_organisasi.length), 10);
-
+    const workSheet = XLSX.utils.json_to_sheet(rows, { origin: "A4" });
     const workBook = XLSX.utils.book_new();
-    const workSheetData = [
-      workSheetColumnName,
-      ...data
+    XLSX.utils.book_append_sheet(workBook, workSheet, "Data Ormawa");
+
+    XLSX.utils.sheet_add_aoa(workSheet, [[
+      { v: "Nama Kegiatan", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+      { v: "Nama Organisasi", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+      { v: "Jumlah Dana", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+      { v: "Ketua Panitia", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+      { v: "Tanggal Pelaksanaan", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+      { v: "Tempat Pelaksanaan", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+
+    ]], { origin: "A4" });
+
+    XLSX.utils.sheet_add_aoa(workSheet, [[
+      { v: "DATA ORGANISASI MAHASISWA FAKULTAS KEDOKTERAN", t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+
+    ]], { origin: "A1" });
+
+    XLSX.utils.sheet_add_aoa(workSheet, [[
+      { v: `BULAN ${proposal.tanggal_pelaksanaan}`, t: "s", s: { font: { name: "Times New Roman", sz: 12, bold: true }, alignment: { wrapText: false, vertical: "center", horizontal: "center" } } },
+
+    ]], { origin: "A2" });
+
+    const width_col1 = rows.reduce((w, r) => Math.max(w, r.nama_kegiatan.length), 20);
+    const width_col2 = rows.reduce((w, r) => Math.max(w, r.nama_organisasi.length), 20);
+    const width_col3 = rows.reduce((w, r) => Math.max(w, r.jumlah_dana.length), 20);
+    const width_col4 = rows.reduce((w, r) => Math.max(w, r.ketua_panitia.length), 20);
+    const width_col5 = rows.reduce((w, r) => Math.max(w, r.tanggal_pelaksanaan.length), 25);
+    const width_col6 = rows.reduce((w, r) => Math.max(w, r.tempat_pelaksanaan.length), 25);
+
+    workSheet["!cols"] = [{ wch: width_col1 },
+    { wch: width_col2 },
+    { wch: width_col3 },
+    { wch: width_col4 },
+    { wch: width_col5 },
+    { wch: width_col6 },
     ];
 
-    workSheetData["!cols"] = [{ wch: max_width }];
+    workSheet["!rows"] = [
+      { hpx: 20 }
+    ]
 
-    const workSheet = XLSX.utils.aoa_to_sheet(workSheetData);
-    XLSX.utils.book_append_sheet(workBook, workSheet, workSheetName);
-    XLSX.writeFile(workBook, path.resolve(filepath));
-    return true
-  }
+    const merge = [
+      { s: { r: 0, c: 0 }, e: { r: 0, c: 5 } },
+      { s: { r: 1, c: 0 }, e: { r: 1, c: 5 } },
+    ];
 
-  try {
-    exportDataToExcel(proposal, workSheetColumnName, workSheetName, filepath);
+    workSheet["!merges"] = merge;
+
+    XLSX.writeFile(workBook,"DATA EXPORT.xlsx", { compression: true });
+
     res.status(200).json({ msg: "Export Data successfuly" });
   } catch (error) {
     console.log(error);
   }
-
-
-
-
 }
