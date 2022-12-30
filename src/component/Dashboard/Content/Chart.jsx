f95fae3c322df278ecb00025c8742e7f80c6b683
briandsmth <briandsmith22@gmail.com>
2022-12-30 08:51:22 +0800

    fix chart dynamical label from database

--- src/component/Dashboard/Content/Chart.jsx
+++ src/component/Dashboard/Content/Chart.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
+
 function Chart() {
+  const [proposals, setProposals] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  const [ormawa, setOrmawa] = useState([]);
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+
+  const dataFetch = async () => {
+    const dataormawa = [];
+    await axios.get(`http://localhost:3000/proposal?search_query=${keyword}`).then(res => {
+      console.log(res);
+      for (const dataObj of res.data.result) {
+        dataormawa.push(dataObj.nama_organisasi)
+      }
+      setOrmawa(dataormawa);
+    }).catch(err => {
+      console.log(err);
+    });
+    console.log(dataormawa);
+  };
+
   const data = {
-    labels: ['Ormawa 1', 'Ormawa 2', 'Ormawa 3', 'Ormawa 4', 'Ormawa 5'],
+    labels: ormawa,
     datasets: [
       {
-        data: [8, 7, 8, 6, 8, 14],
+        label: "Nama Ormawa",
+        data: [8, 7],
         backgroundColor: ' rgb(10, 124, 92)',
       },
     ],
   };
+
   const options = {
     responsive: true,
     plugins: {
