a5c852e726a4064d678166c66eae962537aaaa1d
wawanharianto <wawanharianto696@gmail.com>
2023-01-08 22:57:25 +0800

    form edit propalsal

--- src/component/Dashboard/Users/PengajuanProposal/EditProposal.jsx
+++ src/component/Dashboard/Users/PengajuanProposal/EditProposal.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import './PengajuanProposal.css';
 import axios from 'axios';
-import { useNavigate, useParams } from "react-router-dom";
-
+import { useNavigate, useParams } from 'react-router-dom';
+import './EditProposal.css';
 function UpdateProposal() {
   const [kegiatan, setKegiatan] = useState('');
   const [organisasi, setOrganisasi] = useState('');
@@ -89,94 +88,91 @@ function UpdateProposal() {
           <h2>Pengajual Proposal</h2>
           <p>Add Proposal</p>
         </div>
-        <div className="formaddProp">
-          <div className="headForm">
-            <p>Add Proposal</p>
-            <i class="fa-solid fa-chevron-down"></i>
+        <div className="container-formAddProp">
+          <div className="formaddProp">
+            <div className="headForm">
+              <p>Add Proposal</p>
+              <i class="fa-solid fa-chevron-down"></i>
+            </div>
+            <form onSubmit={updateProposal} className="addProposal" action="">
+              <hr className="line" />
+              <div className="finput">
+                <p>Nama Kegiatan</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Nama Kegiatan" value={kegiatan} onChange={(e) => setKegiatan(e.target.value)}></input>
+                  <p className="kosong">Nama Kegiatan</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Nama Organisasi</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Nama Organisasi" value={organisasi} onChange={(e) => setOrganisasi(e.target.value)}></input>
+                  <p className="kosong">Nama Organisasi</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Jumlah Dana yang Diajukan</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Jumlah Dana yang Diajukan" value={dana} onChange={(e) => setDana(e.target.value)}></input>
+                  <p className="kosong">Jumlah Dana yang Diajukan</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Nama Ketua Panitia</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Nama Ketua Panitia" value={ketupat} onChange={(e) => setKetupat(e.target.value)}></input>
+                  <p className="kosong">Nama Ketua Panitia</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Nomor Hp</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Nomor Hp" value={nohp} onChange={(e) => setNohp(e.target.value)}></input>
+                  <p className="kosong">Nomor HP</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Tanggal Pelaksanaan</p>
+                <div className="contInput">
+                  <input type="date " placeholder="Tanggal Pelaksanaan" value={tanggal} onChange={(e) => setTanggal(e.target.value)}></input>
+                  <p className="kosong">Tanggal Pelaksanaan</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Tempat Pelaksanaan</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Tempat Pelaksanaan" value={tempat} onChange={(e) => setTempat(e.target.value)}></input>
+                  <p className="kosong">Tempat Pelaksanaan</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Nomor Ketua Umum</p>
+                <div className="contInput">
+                  <input type="text" placeholder="Nomor Ketua Umum" value={ketum} onChange={(e) => setKetum(e.target.value)}></input>
+                  <p className="kosong">Nomor Ketua Umum</p>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Proposal</p>
+                <div className="contInput">
+                  <input type="file" name="file" onChange={loadFile}></input>
+                </div>
+              </div>
+              <div className="finput">
+                <p>Status</p>
+                <div className="contInput">
+                  <button disabled className="status">
+                    <i class="fa-solid fa-circle-exclamation"></i> permohonan
+                  </button>
+                </div>
+              </div>
+              <div className="fbtn-form">
+                <button type="submit" className="Ajukan" onClick={handleClose}>
+                  <i class="fa-solid fa-location-arrow"></i>Update
+                </button>
+              </div>
+            </form>
           </div>
-          <form onSubmit={updateProposal} className="addProposal" action=''>
-            <hr className="line" />
-            <div className="finput">
-              <p>Nama Kegiatan</p>
-              <input type="text" placeholder="Nama Kegiatan" value={kegiatan} onChange={(e) => setKegiatan(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Nama Kegiatan</p>
-            </div>
-            <div className="finput">
-              <p>Nama Organisasi</p>
-              <input type="text" placeholder="Nama Organisasi" value={organisasi} onChange={(e) => setOrganisasi(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Nama Organisasi</p>
-            </div>
-            <div className="finput">
-              <p>Jumlah Dana yang Diajukan</p>
-              <input type="text" placeholder="Jumlah Dana yang Diajukan" value={dana} onChange={(e) => setDana(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Jumlah Dana yang Diajukan</p>
-            </div>
-            <div className="finput">
-              <p>Nama Ketua Panitia</p>
-              <input type="text" placeholder="Nama Ketua Panitia" value={ketupat} onChange={(e) => setKetupat(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Nama Ketua Panitia</p>
-            </div>
-            <div className="finput">
-              <p>Nomor Hp</p>
-              <input type="text" placeholder="Nomor Hp" value={nohp} onChange={(e) => setNohp(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Nomor HP</p>
-            </div>
-            <div className="finput">
-              <p>Tanggal Pelaksanaan</p>
-              <input type="text" placeholder="Tanggal Pelaksanaan" value={tanggal} onChange={(e) => setTanggal(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Tanggal Pelaksanaan</p>
-            </div>
-            <div className="finput">
-              <p>Tempat Pelaksanaan</p>
-              <input type="text" placeholder="Tempat Pelaksanaan" value={tempat} onChange={(e) => setTempat(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Tempat Pelaksanaan</p>
-            </div>
-            <div className="finput">
-              <p>Nomor Ketua Umum</p>
-              <input type="text" placeholder="Nomor Ketua Umum" value={ketum} onChange={(e) => setKetum(e.target.value)}></input>
-            </div>
-            <div className="finput">
-              <p className="hide">....</p>
-              <p className="kosong">Nomor Ketua Umum</p>
-            </div>
-            <div className="finput">
-              <p>Proposal</p>
-              <input type="file" name='file' onChange={loadFile}></input>
-            </div>
-
-            <div className="finput">
-              <p>Status</p>
-              <button disabled>
-                <i class="fa-solid fa-circle-exclamation"></i> permohonan
-              </button>
-            </div>
-            <div className="fbtn-form">
-              <button type='submit' className="Ajukan" onClick={handleClose}>
-                <i class="fa-solid fa-location-arrow"></i>Update
-              </button>
-            </div>
-          </form>
         </div>
       </div>
       <div className="popUp pophide">
